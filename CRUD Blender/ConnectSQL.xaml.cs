6d36abf8a1151a582158880774070125c87316d3
Tejas Patel <tejas.patel@teck.com>
2021-01-07 14:09:58 -0700

    Add project files.

---
+++ CRUD Blender/ConnectSQL.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+//
+using System.Data;
+using System.Windows;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace CRUD_Blender {
+    /// <summary>
+    /// Interaction logic for ConnectSQL.xaml
+    /// </summary>
+    public partial class ConnectSQL : Window {
+        public ConnectSQL() {
+            InitializeComponent();
+        }
+
+        private void Windows_Loaded(object sender, RoutedEventArgs e) {
+            DbClass.openConnection();
+            DbClass.sql = "SELECT * FROM RawMasterComponents";
+            DbClass.cmd.CommandType = CommandType.Text;
+            DbClass.cmd.CommandText = DbClass.sql;
+
+            DbClass.da = new SqlDataAdapter(DbClass.cmd);
+            DbClass.dt = new DataTable();
+            DbClass.da.Fill(DbClass.dt);
+
+            FelixDataGrid.ItemsSource = DbClass.dt.TableName;
+            HeadingGrid.ItemsSource = DbClass.dt.DefaultView;
+
+            DbClass.closeConnection();
+        }
+    }
+}
---
+++ CRUD Blender/SQLServerConnection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//
+using System.Data;
+using System.Windows;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace CRUD_Blender {
+    class DbClass {
+
+        public static string GetConnectionStrings() {
+            string SubMainMenuString = ConfigurationManager.ConnectionStrings["conString"].ToString();
+            return SubMainMenuString;
+        }
+
+        
+        public static string sql;
+        public static SqlConnection con = new SqlConnection();
+        public static SqlCommand cmd = new SqlCommand("", con);
+        public static SqlDataReader rd;
+        public static DataTable dt;
+        public static SqlDataAdapter da;
+
+        public static void openConnection() {
+            try {
+                if(con.State == ConnectionState.Closed) {
+                    con.ConnectionString = GetConnectionStrings();
+                    con.Open();
+                }
+            }
+            catch(Exception ex) {
+                MessageBox.Show("Connection Failed: The system failed to establish a connection." + Environment.NewLine +
+                    "Descriptions: " + ex.Message.ToString(), "C# WPF Connect to SQL Server", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        public static void closeConnection() {
+            try {
+                if (con.State == ConnectionState.Open) {
+                    con.Close();
+                }
+            }
+            catch(Exception) {
+                //
+            }
+        }
+    }
+}
